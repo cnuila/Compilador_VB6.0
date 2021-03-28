c9aee0426bf85e9eef8423c35cdf25aa55079d8b
Carlos Nuila <cnuila14@icloud.com>
2021-03-28 16:57:34 -0600

    Errores

--- compileCUP.sh
+++ compileCUP.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar "../java-cup-bin-11b/java-cup-11b.jar" Syntax.cup
+java -jar "./java-cup-bin-11b/java-cup-11b.jar" Syntax.cup
--- compilerun.sh
+++ compilerun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-javac -cp .:../java-cup-bin-11b/java-cup-11b.jar:../jflex-1.8.2/lib/jflex-full-1.8.2.jar Main.java Lexer.java sym.java parser.java
-java -cp .:../java-cup-bin-11b/java-cup-11b-runtime.jar Main entrada.txt
+javac -cp .:./java-cup-bin-11b/java-cup-11b.jar:./jflex-1.8.2/lib/jflex-full-1.8.2.jar Main.java Lexer.java sym.java parser.java
+java -cp .:./java-cup-bin-11b/java-cup-11b-runtime.jar Main entrada.txt

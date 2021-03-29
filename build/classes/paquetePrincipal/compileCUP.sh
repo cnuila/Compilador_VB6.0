72228a0b9b3642029339854246fa3c30d1ab69f3
Carlos Nuila <cnuila14@icloud.com>
2021-03-29 01:22:35 -0600

    Avance

--- build/classes/paquetePrincipal/compileCUP.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -jar "../../java-cup-11b.jar" Syntax.cup
--- build/classes/paquetePrincipal/compilerun.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-javac -cp .:../../java-cup-11b.jar:../../jflex-full-1.8.2.jar Main2.java Lexer.java sym.java parser.java Nodo.java GraphViz.java
-java -cp .:../../java-cup-11b-runtime.jar Main2 entrada.txt

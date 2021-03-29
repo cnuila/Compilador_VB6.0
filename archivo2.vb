d54a721f71402dbe6352a3816ca0196d4d46a45f
Carlos Nuila <cnuila14@icloud.com>
2021-03-29 09:47:32 -0600

    Archivos

---
+++ archivo2.vb
@@ -0,0 +1,57 @@
+Module VbModule
+
+        dim a as Integer = (2+3)/(3+5)
+
+        function MyFunction(j As Integer) As Integer
+                If count = 0 Then
+                                dim count2 as Integer = 0
+                                x = (2+3)/(3+5)
+                                p = console.readline()
+                end if
+        End Function
+
+        Sub Main()
+                If count = 0 Then
+                        a = 0 + 4 * 5 + (2+3) * (3+5)
+                        
+                        If count = prueba(3) Then
+                                dim count2 as Integer = f + 3
+                                a = "a" & a
+                                c = a
+                                h = prueba(9)
+                        elseif a + b >= 6 / 3  then
+                                dim count2 as Integer = 0
+                                Dim a as Integer, x, y As String, i As Integer
+                                Dim a,d as Integer
+                                x = "Hola jeej" & " ADios" & a
+                                y = "Aqui empiezo"&_
+                                        "Aqui sigo"
+                                console.writeline("Hola Mundo")
+                                console.writeline(b)
+                                console.writeline(a & x)
+                        else                                             
+                        Exit Sub 
+                        End If           
+                End If                
+        end sub
+
+        sub prueba (j as Integer)
+                If count = 0 Then
+                        a = 0 + 4 + 5 + (2+3) * (3+5)
+                        If count = 0 Then
+                                dim count2 as Integer = 0                                                  
+                        End If           
+                End If                
+        end sub
+
+        function MyFunction(j,x,y As Integer, j As Integer,j As Integer,j As Integer) As Integer
+                If count = 2 Then
+                        MyFunction(2,3,2,j,x,r,e)
+                        dim count2 as Integer = 2  
+                        x = "Hola jeej" & " ADios"
+                end if
+                return a
+        End Function
+
+        dim a as Integer = 0         
+End Module

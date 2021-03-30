0a5c25e998261a07a0291c7e358832597131efa3
Carlos Nuila <cnuila14@icloud.com>
2021-03-30 11:13:55 -0600

    Correcciones

--- archivoError1.vb
+++ archivoError1.vb
@@ -1,8 +1,16 @@
 Module VbModule
 
+        function factorial(n As Integer) As Integer
+                n = 0 Then
+                    return 0
+                else
+                    return factorial(n-1)
+                end if
+        End Function
+
         Sub Main()
                 console.writeline("Ingrese el n para calcular el factorial")
-                Dim n as Integer ; 
+                Dim n as Integer
                 n = console.readline()
                 factorial(n)                
         end sub
